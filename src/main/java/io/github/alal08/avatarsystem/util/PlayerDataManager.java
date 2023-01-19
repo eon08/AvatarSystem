124474841b99073c33bb38b708d1ecaea1983a2f
nyeyo <leeyunhyeong115@gmail.com>
2023-01-20 00:43:30 +0900

    플레이어 데이터 관리자

---
+++ src/main/java/io/github/alal08/avatarsystem/util/PlayerDataManager.java
@@ -0,0 +1,34 @@
+package io.github.alal08.avatarsystem.util;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.EntityPlayer;
+import net.minecraft.server.level.WorldServer;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.craftbukkit.v1_19_R2.CraftServer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PlayerDataManager {
+
+    public static @Nullable Player loadPlayer(@NotNull final OfflinePlayer offline) {
+        if (!offline.hasPlayedBefore()) {
+            return null;
+        }
+        GameProfile profile = new GameProfile(offline.getUniqueId(),
+                offline.getName() != null ? offline.getName() : offline.getUniqueId().toString());
+        MinecraftServer server = ((CraftServer) Bukkit.getServer()).getServer();
+        WorldServer worldServer = server.C();
+        if (worldServer == null) {
+            return null;
+        }
+        EntityPlayer entity = new EntityPlayer(server, worldServer, profile);
+        Player target = entity.getBukkitEntity();
+        if (target != null) {
+            target.loadData();
+        }
+        return target;
+    }
+}
