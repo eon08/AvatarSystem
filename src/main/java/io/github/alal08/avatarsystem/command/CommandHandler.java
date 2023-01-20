36b6da6b98a2c2dd56f7744d076c81e71a46b095
nyeyo <leeyunhyeong115@gmail.com>
2023-01-20 22:20:30 +0900

    CommandHandler.java 추가 및 사용되지 않는 코드 주석화

---
+++ src/main/java/io/github/alal08/avatarsystem/command/CommandHandler.java
@@ -0,0 +1,10 @@
+package io.github.alal08.avatarsystem.command;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public class CommandHandler {
+
+    public CommandHandler(@NotNull JavaPlugin javaPlugin) {
+    }
+}
--- src/main/java/io/github/alal08/avatarsystem/listener/NPCListener.java
+++ src/main/java/io/github/alal08/avatarsystem/listener/NPCListener.java
@@ -3,7 +3,6 @@ package io.github.alal08.avatarsystem.listener;
 import net.citizensnpcs.api.event.NPCDeathEvent;
 import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.api.trait.trait.Inventory;
-import net.citizensnpcs.api.trait.trait.Owner;
 import org.bukkit.GameRule;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
@@ -11,14 +10,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.UUID;
-
 public class NPCListener implements Listener {
 
     @EventHandler
     public void onNPCDeath(@NotNull NPCDeathEvent event) {
         NPC npc = event.getNPC();
-        UUID ownerUUID = npc.getOrAddTrait(Owner.class).getOwnerId();
+        //UUID ownerUUID = npc.getOrAddTrait(Owner.class).getOwnerId();
         World world = npc.getEntity().getWorld();
         if (Boolean.FALSE.equals(world.getGameRuleValue(GameRule.KEEP_INVENTORY))) {
             ItemStack[] itemStacks = npc.getOrAddTrait(Inventory.class).getContents();
